043f98576756f565b9846974459a15185d77bd7c
selenaassou <aselenateaches@gmail.com>
2023-08-25 20:00:26 +0200

    Added api key in the termial as export and it works! I see a response once running the py file.

--- app/copykitt.py
+++ app/copykitt.py
@@ -4,4 +4,6 @@ import openai
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+print(os.getenv("OPENAI_API_KEY"))
 chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
+print(chat_completion)
